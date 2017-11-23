d34656ceba5f6e5fa5829c6afe16bb801168d55e
Zaven Muradyan <zaven@skepsi.me>
2017-11-22 22:29:05 -0800

    Refactor the entity script a bit.

--- char.gd
+++ char.gd
@@ -1,14 +1,7 @@
 extends "res://scripts/entity.gd"
 
-export (int) var SPEED = 160 # Pixels/second
-
-# Sprites
-var animation_player
-var last_dir = "down"
-
 func _ready():
 	set_fixed_process(true)
-	animation_player = get_node("AnimationPlayer")
 
 func _fixed_process(delta):
 	var motion = Vector2()
@@ -26,17 +19,6 @@ func _fixed_process(delta):
 	if Input.is_action_pressed("ui_right"):
 		motion.x += 1
 		dir = "right"
-	
-	if dir and dir != last_dir:
-		animation_player.play(dir + "_idle")
-	last_dir = dir
 
 	motion = motion.normalized() * SPEED * delta
-	motion = move(motion)
-	
-	# Make character slide nicely through the world
-	var slide_attempts = 4
-	while is_colliding() and slide_attempts > 0:
-		motion = get_collision_normal().slide(motion)
-		motion = move(motion)
-		slide_attempts -= 1
+	move_entity(motion, dir)
--- scripts/entity.gd
+++ scripts/entity.gd
@@ -1,15 +1,64 @@
 extends KinematicBody2D
 
-# Common health management.
+# Common entity (player, enemy) management.
 
 signal health_changed
 signal died
 
 var health
-export var max_health = 10
+export (int) var MAX_HEALTH = 100
+export (int) var SPEED = 100 # Pixels/second
+
+onready var animation_player = get_node("AnimationPlayer")
+
+const STATE_IDLE = 0
+const STATE_MOVE = 1
+const STATE_ATTACK = 2
+const STATE_DYING = 3
+const STATE_DEAD = 4
+
+var current_state = STATE_IDLE
+var previous_state = STATE_IDLE
+
+# Valid dirs are "up", "down", "left", "right", and null, denoting idleness.
+var current_dir = null
+var previous_dir = null
 
 func _ready():
-	health = max_health
+	health = MAX_HEALTH
+
+func change_state(new_state):
+	if new_state == current_state:
+		return
+	print("State changed to ", new_state)
+	previous_state = current_state
+	current_state = new_state
+
+# Moves the entity in a given direction.
+#   motion: A Vector2() containing x/y motion.
+#   dir: One of "up", "down", "left", "right", used for animation.
+#        Can also be null to designate no direction (idle)
+func move_entity(motion, dir):
+	previous_dir = current_dir
+	current_dir = dir
+
+	if dir and dir != previous_dir:
+		change_state(STATE_MOVE)
+		# TODO: Change this to *_move.
+		animation_player.play(dir + "_idle")
+	else:
+		change_state(STATE_IDLE)
+		if previous_dir:
+			animation_player.play(previous_dir + "_idle")
+
+	motion = move(motion)
+
+	# Make character slide nicely through the world
+	var slide_attempts = 4
+	while is_colliding() and slide_attempts > 0:
+		motion = get_collision_normal().slide(motion)
+		motion = move(motion)
+		slide_attempts -= 1
 
 func take_damage(damage):
 	# TODO: Add state management so that take_damage/heal are no-ops
@@ -22,7 +71,7 @@ func take_damage(damage):
 		emit_signal("health_changed", health)
 
 func heal(amount):
-	health = min(health + amount, max_health)
+	health = min(health + amount, MAX_HEALTH)
 
 func health_ratio():
-	return health / max_health
+	return health / MAX_HEALTH
