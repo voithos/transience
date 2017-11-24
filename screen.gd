43e96b02166d2ea40bc52664c7cf75afd8c35284
Zaven Muradyan <zaven@skepsi.me>
2017-11-23 17:04:05 -0800

    Launch in windowed mode by default for debug builds.

--- screen.gd
+++ screen.gd
@@ -2,7 +2,10 @@ extends Node
 
 func _ready():
 	set_process_input(true)
-	OS.set_window_fullscreen(true)
+	if not OS.is_debug_build():
+		OS.set_window_fullscreen(true)
+	else:
+		OS.set_window_size(Vector2(800, 600))
 
 func _input(event):
 	if event.type == InputEvent.KEY and event.is_pressed() and event.scancode == KEY_ESCAPE:
