39f02f1f0c87180a13ffa52e14d0ee79a54f11e0
Zaven Muradyan <zaven@skepsi.me>
2017-11-23 17:03:49 -0800

    Add tweening for UI bars.

--- scenes/ui/ui_bars.gd
+++ scenes/ui/ui_bars.gd
@@ -1,8 +1,11 @@
-
 extends CanvasLayer
+
+const BAR_TWEEN_TIME = 0.5
 
 onready var health_bar = get_node("HealthBar")
 onready var flux_bar = get_node("FluxBar")
+onready var health_tween = get_node("HealthBarTween")
+onready var flux_tween = get_node("FluxBarTween")
 
 func _ready():
 	setup_listeners()
@@ -19,7 +22,11 @@ func setup_listeners():
 	flux_bar.set_value(char.flux_ratio() * 100)
 
 func on_health_changed(ratio):
-	health_bar.set_value(ratio * 100)
+	health_tween.interpolate_property(health_bar, "range/value", health_bar.get_value(), ratio * 100, \
+			BAR_TWEEN_TIME, Tween.TRANS_QUAD, Tween.EASE_OUT)
+	health_tween.start()
 
 func on_flux_changed(ratio):
-	flux_bar.set_value(ratio * 100)
+	flux_tween.interpolate_property(flux_bar, "range/value", flux_bar.get_value(), ratio * 100, \
+			BAR_TWEEN_TIME, Tween.TRANS_QUAD, Tween.EASE_OUT)
+	flux_tween.start()
