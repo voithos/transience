ed9ef4571edc7bb0eccc41f0fab32bf18726ff6a
Zaven Muradyan <zaven@skepsi.me>
2017-11-23 22:00:58 -0800

    Add throwback ability.

--- scenes/char/char.gd
+++ scenes/char/char.gd
@@ -2,9 +2,16 @@ extends "res://scripts/flux_entity.gd"
 
 func _ready():
 	set_fixed_process(true)
+	set_process_input(true)
 	add_to_group("char")
 
 func _fixed_process(delta):
+	react_to_motion_controls(delta)
+
+func _input(event):
+	react_to_action_controls(event)
+
+func react_to_motion_controls(delta):
 	var motion = Vector2()
 	var dir = null
 	
@@ -23,3 +30,8 @@ func _fixed_process(delta):
 
 	motion = motion.normalized() * SPEED * delta
 	move_entity(motion, dir)
+
+func react_to_action_controls(event):
+	if event.is_action_pressed("trans_accept"):
+		if can_throwback(THROWBACK_STEPS):
+			throwback(THROWBACK_STEPS)
--- scripts/flux_entity.gd
+++ scripts/flux_entity.gd
@@ -5,8 +5,10 @@ signal flux_changed
 export (int) var MAX_FLUX = 100
 onready var flux = 0
 
-const FLUX_PER_DELTA_TIME = 1 # Arbitrary, for now
+const FLUX_PER_THROWBACK_STEP = 1 # Arbitrary, for now
 const FLUX_GAIN_RATE = .2 # Arbitrary, for now
+# TODO: Make delta-throwback based on how long button is held.
+const THROWBACK_STEPS = 50
 
 const MAX_THROWBACKS = 256
 var throwback_positions = []
@@ -24,6 +26,7 @@ func move_entity(motion, dir):
 	if motion.length_squared() and not remainder.length_squared():
 		heal_flux(motion.length_squared() * FLUX_GAIN_RATE)
 		cache_throwback_position()
+	return remainder
 	
 func heal_flux(amount):
 	var previous_flux = flux
@@ -35,18 +38,28 @@ func heal_flux(amount):
 func flux_ratio():
 	return flux / MAX_FLUX
 
-func can_throwback(delta):
-	return (delta * FLUX_PER_DELTA_TIME) <= flux
+
+func can_throwback(steps):
+	var enough_flux = (steps * FLUX_PER_THROWBACK_STEP) <= flux
+	var enough_throwback = steps <= throwback_count
+	return enough_flux and enough_throwback
 
 # Caches the throwback position in the cyclic throwback array.
 func cache_throwback_position():
 	throwback_positions[throwback_i] = get_pos()
 	throwback_i = (throwback_i + 1) % MAX_THROWBACKS
 	throwback_count = min(throwback_count + 1, MAX_THROWBACKS)
-	print(throwback_i)
 
-func throwback(delta):
-	assert(can_throwback(delta))
-	flux -= delta * FLUX_PER_DELTA_TIME
+# 
+func throwback(steps):
+	assert(can_throwback(steps))
+	flux -= steps * FLUX_PER_THROWBACK_STEP
 	emit_signal("flux_changed", flux_ratio())
-	# TODO: Actually perform the throwback.
+	
+	# GDScript's modulo operator doesn't work on negatives, so "wrap" manually.
+	throwback_i -= steps
+	if throwback_i < 0:
+		throwback_i += MAX_THROWBACKS
+	throwback_count -= steps
+	
+	set_pos(throwback_positions[throwback_i])
