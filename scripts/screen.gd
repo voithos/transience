016156ad21df5eaf718ad8999efb0bcc25751d9b
Zaven Muradyan <zaven@skepsi.me>
2017-11-23 20:47:10 -0800

    Minor cleanup.

--- scripts/screen.gd
+++ scripts/screen.gd
@@ -1,11 +1,13 @@
 extends Node
 
+const DEBUG_WINDOW_SIZE = Vector2(800, 600)
+
 func _ready():
 	set_process_input(true)
 	if not OS.is_debug_build():
 		OS.set_window_fullscreen(true)
 	else:
-		OS.set_window_size(Vector2(800, 600))
+		OS.set_window_size(DEBUG_WINDOW_SIZE)
 
 func _input(event):
 	if event.type == InputEvent.KEY and event.is_pressed() and event.scancode == KEY_ESCAPE:
