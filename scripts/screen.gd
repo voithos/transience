12022de1a1e5e66498ccdf6cb1f30da9af086ee0
Zaven Muradyan <zaven@skepsi.me>
2018-02-10 21:16:58 -0800

    Improve size handling code.

--- scripts/screen.gd
+++ scripts/screen.gd
@@ -1,6 +1,6 @@
 extends Node
 
-const DEBUG_WINDOW_SIZE = Vector2(800, 600)
+const DEFAULT_DEBUG_WINDOW_SIZE = Vector2(800, 600)
 var is_paused = false
 
 func _ready():
@@ -8,7 +8,13 @@ func _ready():
 	if not OS.is_debug_build():
 		OS.set_window_fullscreen(true)
 	else:
-		OS.set_window_size(DEBUG_WINDOW_SIZE)
+		var size = DEFAULT_DEBUG_WINDOW_SIZE
+		var width = ProjectSettings.get_setting("display/window/size/test_width")
+		var height = ProjectSettings.get_setting("display/window/size/test_height")
+
+		if width > 0 and height > 0:
+			size = Vector2(width, height)
+		OS.set_window_size(size)
 
 func _input(event):
 	if event is InputEventKey and event.is_pressed() and event.scancode == KEY_ESCAPE:
