b989d8ca0e346afaf8f41043c3dc99e7cea1b2d2
Zaven Muradyan <zaven@skepsi.me>
2018-04-22 22:23:47 -0700

    Add full screen hotkey for debug builds.

--- scripts/screen.gd
+++ scripts/screen.gd
@@ -1,6 +1,6 @@
 extends Node
 
-const DEFAULT_DEBUG_WINDOW_SIZE = Vector2(800, 600)
+const DEFAULT_DEBUG_WINDOW_SIZE = Vector2(960, 540)
 var is_paused = false
 
 func _ready():
@@ -14,13 +14,16 @@ func _ready():
 
 		if width > 0 and height > 0:
 			size = Vector2(width, height)
-		OS.set_window_size(size)
+		OS.window_size = size
 
 func _input(event):
-	if event is InputEventKey and event.is_pressed() and event.scancode == KEY_ESCAPE:
-		get_tree().quit()
+	if event is InputEventKey and event.is_pressed():
+		if event.scancode == KEY_ESCAPE:
+			get_tree().quit()
 
-	if OS.is_debug_build():
-		if event is InputEventKey and event.is_pressed() and event.scancode == KEY_SPACE:
-			is_paused = not is_paused
-			get_tree().set_pause(is_paused)
+		if OS.is_debug_build():
+			if event.scancode == KEY_SPACE:
+				is_paused = not is_paused
+				get_tree().set_pause(is_paused)
+			if event.scancode == KEY_F11:
+				OS.window_fullscreen = not OS.window_fullscreen
