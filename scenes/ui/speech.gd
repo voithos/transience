1429f25d736304246f42e9684deadaa144d890b9
Zaven Muradyan <zaven@skepsi.me>
2018-06-17 13:55:31 -0700

    Add multi page speech support.

--- scenes/ui/speech.gd
+++ scenes/ui/speech.gd
@@ -1,5 +1,7 @@
 extends Sprite
 
+signal speech_completed
+
 onready var timer = get_node("Timer")
 const TIMER_NORMAL = 0.05
 const TIMER_DELAYED = 1.0
@@ -11,22 +13,46 @@ var speech_data = ["[center]It's dangerous to go alone...\nYou have no chance to
 var page = 0
 
 func _ready():
-	begin_if_ready()
+	start_if_ready()
+
+func _input(event):
+	# TODO: Also react to the other buttons as well.
+	if event.is_action_pressed("trans_accept"):
+		if not is_page_complete():
+			# Complete the page immediately.
+			label.visible_characters = label.get_total_character_count()
+		elif page < len(speech_data) - 1:
+			# Move on to the next page.
+			page += 1
+			start_current_page()
+		else:
+			# Close the dialog.
+			emit_signal("speech_completed")
+			queue_free()
 
 func init(speech_data):
 	self.speech_data = speech_data
-	begin_if_ready()
+	start_if_ready()
 
-func begin_if_ready():
+func start_if_ready():
 	if speech_data:
 		timer.wait_time = TIMER_NORMAL
 		timer.connect("timeout", self, "on_timeout")
 		timer.start()
 		
-		label.bbcode_text = speech_data[page]
-		label.visible_characters = 0
+		start_current_page()
+
+func start_current_page():
+	# Reset timer speed in case we transitioned in the middle
+	# of a slow character.
+	timer.wait_time = TIMER_NORMAL
+	label.bbcode_text = speech_data[page]
+	label.visible_characters = 0
+
+func is_page_complete():
+	return label.visible_characters == label.get_total_character_count()
 
 func on_timeout():
 	# TODO: Add delay support.
-	if label.visible_characters < len(speech_data[page]):
+	if not is_page_complete():
 		label.visible_characters = label.visible_characters + 1
