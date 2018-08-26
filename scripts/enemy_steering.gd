2516dd30904ca45d4a3b9db9d087e3c95a2cf7a6
Zaven Muradyan <zaven@skepsi.me>
2018-08-26 10:05:36 -0700

    Add arrival steering.

--- scripts/enemy_steering.gd
+++ scripts/enemy_steering.gd
@@ -6,5 +6,5 @@ onready var player_controller = get_node("/root/player_controller")
 onready var steering = get_node("/root/steering")
 
 func seek_player(enemy):
-	return steering.seek(enemy.global_position, player_controller.get_player_pos(), \
+	return steering.arrival(enemy.global_position, player_controller.get_player_pos(), 100,\
 			enemy.SPEED, enemy.current_motion, enemy.STEERING_FORCE, enemy.MASS)
--- scripts/steering.gd
+++ scripts/steering.gd
@@ -7,19 +7,31 @@ extends Node
 # Returns the new motion vector.
 func seek(position, target, max_speed, current_motion, max_force, mass):
 	var desired_motion = (target - position).normalized() * max_speed
-	return _steer(desired_motion, max_speed, current_motion, max_force, mass)
+	var steering = desired_motion - current_motion
+	return _steer(steering, max_speed, current_motion, max_force, mass)
 
 # Flees from the target, naturally altering the heading.
 # Returns the new motion vector.
 func flee(position, target, max_speed, current_motion, max_force, mass):
 	var desired_motion = (position - target).normalized() * max_speed
-	return _steer(desired_motion, max_speed, current_motion, max_force, mass)
-
-# Steers the current_motion toward the desired_motion, taking into account force and mass.
-# Used by seek() and flee().
-func _steer(desired_motion, max_speed, current_motion, max_force, mass):
 	var steering = desired_motion - current_motion
-	
+	return _steer(steering, max_speed, current_motion, max_force, mass)
+
+# Arrives at the target's position, slowing down after passing slow_radius.
+# Returns the new motion vector.
+func arrival(position, target, slow_radius, max_speed, current_motion, max_force, mass):
+	var desired_motion = target - position
+	var distance = desired_motion.length()
+
+	desired_motion = desired_motion.normalized() * max_speed
+	if distance < slow_radius:
+		desired_motion *= (distance / slow_radius)
+	var steering = desired_motion - current_motion
+	return _steer(steering, max_speed, current_motion, max_force, mass)
+
+# Applies a steering to the current_motion, taking into account force and mass.
+# Used by steering functions.
+func _steer(steering, max_speed, current_motion, max_force, mass):
 	# Limit the steering vector to some max length.
 	steering = steering.clamped(max_force)
 	# Also limit it by the entity's mass.
